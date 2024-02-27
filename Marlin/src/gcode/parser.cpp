26a60dbabdcd281e59d259764f8a00368736da79
FRUCHTBA3R <fefafi@netcologne.de>
2024-02-27 02:38:11 +0100

    Fix gcode eol comments
    Fixed gcode end-of-line comments being interpreted as valid parameters.

--- Marlin/src/gcode/parser.cpp
+++ Marlin/src/gcode/parser.cpp
@@ -299,7 +299,14 @@ void GCodeParser::parse(char *p) {
   #endif
   string_arg = nullptr;
   while (const char param = uppercase(*p++)) {  // Get the next parameter. A NUL ends the loop
-
+    
+    // Remove end-of-line comments
+    if (param == ';') {
+      // Leading whitespaces can not be removed as command_args might point here
+      p[-1] = '\0';
+      break;
+    }
+    
     // Special handling for M32 [P] !/path/to/file.g#
     // The path must be the last parameter
     if (param == '!' && is_command('M', 32)) {
